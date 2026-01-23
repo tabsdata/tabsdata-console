3b54302982f004ed24ceed902d9a7d03a8b64302
adaydan-tabsdata <daniel@tabsdata.com>
2026-01-23 00:46:09 -0500

    push

--- src/tdconsole/textual_assets/spinners.py
+++ src/tdconsole/textual_assets/spinners.py
@@ -1,6 +1,4 @@
 from rich.spinner import Spinner
-
-from textual.app import App, ComposeResult
 from textual.widgets import Static
 
 
@@ -11,12 +9,10 @@ class SpinnerWidget(Static):
     ]
 
     def __init__(self, spinner_name: str = "dots", *args, **kwargs) -> None:
-        # Accept id=, classes=, etc from Textual and forward them
         super().__init__("", *args, **kwargs)
         self._spinner = Spinner(spinner_name)
 
     def on_mount(self) -> None:
-        # ~20 FPS is plenty for a terminal spinner
         self.set_interval(1 / 20, self.update_spinner)
 
     def update_spinner(self) -> None:
